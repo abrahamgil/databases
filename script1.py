d93f5c6d0e27e961296f8f203db429fd9c43c08c
Abraham <abrahamgil2@gmail.com>
2020-03-17 05:46:18 -0500

    Added an update function to update values in database

--- script1.py
+++ script1.py
@@ -29,6 +29,12 @@ def delete(item):
     conn.commit()
     conn.close()
 
+def update(quantity,price,item):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?",(quantity,price,item))
+    conn.commit()
+    conn.close()
 
-delete("Wine Glass")
+update(11,6,"Water Glass")
 print(view())
