72b3a4b2c3266a508ea13ba86218fe71991277df
Abraham <abrahamgil2@gmail.com>
2020-03-19 01:31:02 -0500

    updated delete and update functions to work with postgres

--- script2.py
+++ script2.py
@@ -25,19 +25,21 @@ def view():
     return rows
 
 def delete(item):
-    conn = psycopg2.connect("lite.db")
+    conn = psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
     cur = conn.cursor()
-    cur.execute("DELETE FROM store WHERE item=?",(item,))
+    cur.execute("DELETE FROM store WHERE item=%s",(item,))
     conn.commit()
     conn.close()
 
 def update(quantity,price,item):
-    conn = psycopg2.connect("lite.db")
+    conn = psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
     cur = conn.cursor()
-    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?",(quantity,price,item))
+    cur.execute("UPDATE store SET quantity=%s, price=%s WHERE item=%s",(quantity,price,item))
     conn.commit()
     conn.close()
 
 create_table()
-insert("Orange",10,15)
+#insert("Orange",10,15)
+#delete("Orange")
+update(20,15.0,'Apple')
 print(view())
